b573ea81ee696ec4c604fdce0fe1c9d568b07456
Navaneethan Rajasekaran <navaneethan.rajasekaran2@mail.dcu.ie>
2020-08-07 12:42:12 +0100

    updates

---
+++ precache-manifest.9e6dc86a60eb03230a020c466c76286a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf5acad8dd0237291f1519df340d201e",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "8878082c36e3c9ef0fea",
+    "url": "/Portfolio/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "a82124cc315811fb1412",
+    "url": "/Portfolio/static/css/4.54df59d4.chunk.css"
+  },
+  {
+    "revision": "84b991cdad51fd7c43b5",
+    "url": "/Portfolio/static/css/main.1a2504aa.chunk.css"
+  },
+  {
+    "revision": "1a51b72aba16c7cb938c",
+    "url": "/Portfolio/static/js/2.b794abe2.chunk.js"
+  },
+  {
+    "revision": "8878082c36e3c9ef0fea",
+    "url": "/Portfolio/static/js/3.007a8f4e.chunk.js"
+  },
+  {
+    "revision": "a82124cc315811fb1412",
+    "url": "/Portfolio/static/js/4.0f57cdbf.chunk.js"
+  },
+  {
+    "revision": "84b991cdad51fd7c43b5",
+    "url": "/Portfolio/static/js/main.b2975ddf.chunk.js"
+  },
+  {
+    "revision": "972207106a0c6df00580",
+    "url": "/Portfolio/static/js/runtime-main.470a3d48.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/Portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/Portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/Portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "0a77bb92d1fe543bbc3ff25631141b52",
+    "url": "/Portfolio/static/media/insight_logo.0a77bb92.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/Portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/Portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/Portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  }
+]);
